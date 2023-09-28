8a283be1639a7c4565c6a7709c6a327707c1d2ff
ikeicg <ikechukwuemeka60@gmail.com>
2023-09-28 14:33:58 +0100

    setup cors, ejs, etc.

--- app.js
+++ app.js
@@ -1,7 +1,24 @@
-express = require("express");
+// Initialize dependencies
+const express = require("express");
+const cors = require("cors");
+const corsOptions = require("./utils/corsOptions");
 
-app = express();
+// Create Express server
+const app = express();
 
-app.listen(3000, () => {
+// Configure cors middleware
+app.use(cors(corsOptions));
+
+// Set EJS and views as view engine and views folder respectively
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/views");
+
+// Define routes here
+app.get("/", (req, res) => {
+  res.render("login");
+});
+
+// Start the server
+app.listen(process.env.PORT || 3000, () => {
   console.log("Server is Live!!");
 });
---
+++ utils/corsOptions.js
@@ -0,0 +1,5 @@
+module.exports = {
+  origin: "*", // Replace with the allowed origin(s)
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE", // Specify allowed HTTP methods
+  credentials: true, // Allow sending cookies and other credentials
+};
